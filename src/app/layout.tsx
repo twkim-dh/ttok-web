e7e69f9147ccce8f41bd194a5c56ddbc2bfd863e
twkim-dh <tw.kim@dhlm.co.kr>
2026-03-18 16:09:16 +0900

    Fix: add Kakao SDK integrity + crossOrigin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,8 @@ export default function RootLayout({
         </div>
         <Script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.7/kakao.min.js"
+          integrity="sha384-tJkjbtDbvoxO+diRuDtwRO9JXR7pjWnfjfRn5ePUpl7e7RJCxKCwwnfqUAdXh53p"
+          crossOrigin="anonymous"
           strategy="afterInteractive"
         />
       </body>
